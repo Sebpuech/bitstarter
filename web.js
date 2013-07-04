ae18c8f1b97522188701e3f8fd616c354c6e5a57
Ubuntu <ubuntu@ip-172-31-20-217.us-west-2.compute.internal>
2013-07-04 13:38:12 +0000

    Updated web.js to test

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     index = fs.readFileSync('index.html');
     buffer = new Buffer(index,'utf-8');
-    response.send(buffer.toString("utf8",0,20));
+    response.send(buffer.toString("utf8",0,buffer.length));
 });
 
 var port = process.env.PORT || 5000;
