d6e4fd92285fa101f16b4ad783e525431959d7ec
Ubuntu <ubuntu@ip-172-31-20-217.us-west-2.compute.internal>
2013-07-16 09:00:47 +0000

    grader.js done

--- grader.js
+++ grader.js
@@ -24,8 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_DEFAULT = "http://aqueous-springs-4129.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler')
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,27 +37,25 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	$ = cheerio.load(htmlfile);
+		for(var ii in checks) {
+        		var present = $(checks[ii]).length > 0;
+        		out[checks[ii]] = present;
+    			}
+    
+	return out;
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
+   // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
@@ -64,11 +63,14 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --url <html_file>', 'Path to index.html',  HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    rest.get(program.url).on('complete', function(result) {
+	var checkJson = checkHtmlFile(result,  program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+	});	
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
